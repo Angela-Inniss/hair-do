68fab3c8886f955b1fd254156505b3a9eea7b4e4
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-04-05 12:22:59 +0100

    added hairstyle skeleton routes

---
+++ app/controllers/hairstyles_controller.rb
@@ -0,0 +1,23 @@
+class HairstylesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+
+
+  def destroy
+  end
+end
