eedc167defa4e69631adf58af5eb0c829dc82def
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-04-22 11:57:16 +0100

    add styling

--- app/models/hairstyle.rb
+++ app/models/hairstyle.rb
@@ -2,10 +2,9 @@
 class Hairstyle < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
-
   validates :name, presence: true
   validates :description, presence: true
-  # validates :photo_url, presence: true
+ # validates :photo_url, presence: true
  # validates :video_url, presence: true
   validates :category, presence: true
 
