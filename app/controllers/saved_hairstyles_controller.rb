8f645f17ab0df142a521c439c26994f64d2bcee8
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-05-03 09:53:04 +0100

    ameded bookmarks

--- app/controllers/saved_hairstyles_controller.rb
+++ app/controllers/saved_hairstyles_controller.rb
@@ -1,9 +1,10 @@
 class SavedHairstylesController < ApplicationController
+include Pundit
+  after_action :verify_authorized, except: [:create, :destroy]
+
   def create
     # 1. find the hairstyle i want to save
-    @raise
     @hairstyle = Hairstyle.find(params[:hairstyle_id])
-
     # 2. Create a new instance of saved hairstyle and find the id of the user (current user) &  hairstyle
     @saved_hairstyle = SavedHairstyle.new(user: current_user, hairstyle: @hairstyle)
     # 3. save  hairstyle
@@ -12,7 +13,7 @@ class SavedHairstylesController < ApplicationController
         # redirect to hairstyle just liked (hairstyle_path) and the new instance of saved recipe
         # and the saved recipe
         format.html { redirect_to hairstyle_path(@saved_hairstyle.hairstyle) }
-        format.js  # <-- will render `app/views/comments/create.js.erb`
+        format.js  # <-- will render `app/views/saved_hairstyles/create.js.erb`
       end
     else
       respond_to do |format|
@@ -22,11 +23,9 @@ class SavedHairstylesController < ApplicationController
     end
   end
 
-
   def destroy
     # 1. I need to find the recipe I want to unsave
     @saved_hairstyle = SavedHairstyle.find(params[:id])
-    authorize @saved_hairstyle
     # 2. then destroy it
     @saved_hairstyle.destroy
     @hairstyle = @saved_hairstyle.hairstyle
